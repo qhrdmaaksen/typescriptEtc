f9ad6154dc8cf2dadafd951e4b7e604f71a5c54b
vitamin777 <rlaalsdn8@gmail.com>
2022-10-27 23:59:59 +0900

    typescript basic content add commit

---
+++ basics.ts
@@ -0,0 +1,13 @@
+console.log('Hello World');
+const myName: string = 'vitamin';
+console.log(myName);
+
+const multiplication = (num: number) => {
+	return num * num ;
+}
+multiplication(3);
+
+const greet = (person: string) => {
+	return `hello ${person}`
+}
+greet('vitamin');
